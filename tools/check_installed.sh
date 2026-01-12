dd48a579485bbe294818fbc1b67e3de4bafff24b
Aidan Wansbrough <aidanw@lochan.org>
2026-01-12 11:23:28 +0000

    feat: add script to check which euclids a program is installed on

---
+++ tools/check_installed.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+UNRESTRICTED_SERVERS="01 02 04 05 10 18 19 21 23 25 26 27 28 29 30"
+RESTRICTED_SERVERS="03 06 07 08 09 12 17 22 24 32 35 36 37 "
+SLURM_SERVERS="13 14 15 16 20 31 33"
+RETIRED_SERVERS="11 34"
+
+SERVERS=$UNRESTRICTED_SERVERS
+
+cmdToCheck=$1
+
+while true; do
+    read -p "Check whether command '$cmdToCheck' exists on the euclid servers? Y/n " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y/n.";;
+    esac
+done
+
+# Run command & save output to temp directory
+tmpdir=$(mktemp -d)
+cmd="command -v $cmdToCheck" # Command to run on each euclid
+for euclid in $SERVERS; do
+	echo $(if out=$(ssh "euclid-$euclid" -q -o RemoteCommand="$cmd" 2>&1); then
+		echo "$out"
+	else
+		echo "Not installed"
+	fi
+	) > $tmpdir/euclid-$euclid &
+done
+
+# Display outputs:
+n_servers=$(echo $SERVERS | wc --words)
+while :; do
+	done_count=0
+	for euclid in $SERVERS; do
+		if [[ -f "$tmpdir/euclid-$euclid" ]]; then
+			printf "euclid-$euclid: %s\n" "$(tr '\n' ' ' < $tmpdir/euclid-$euclid)"
+			((done_count++))
+		else
+			printf "euclid-$euclid:\n"
+		fi
+	done
+	
+	# Break if all servers have responded
+	((done_count == n_servers)) && break
+	
+	# Move cursor back to top
+	printf "\033[${n_servers}A"
+
+	# Wait before re-checking
+	sleep 0.2
+done
+
+rm -r "$tmpdir"
