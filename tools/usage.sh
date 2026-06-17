56bd7782da4743b3cb07c2749603090a01978004
Aidan Wansbrough <aidanw@lochan.org>
2026-06-17 10:54:51 +0100

    feat: increase robustness to lscpu formatting changes

--- tools/usage.sh
+++ tools/usage.sh
@@ -20,9 +20,9 @@ run_cmd(){
 	ssh "euclid-$euclid" -q -o RemoteCommand="$1"
 }
 for euclid in $SERVERS; do
-	run_cmd "top -bn 5 -d 0.1 -E M" | grep -e '^%Cpu' -e '^MiB Mem' | tail -n 2 > $tmpdir/euclid-$euclid-cpu-mem &
+	run_cmd "top -bn 5 -d 0.1 -E M" | grep -e '^%Cpu(s)\s*:' -e '^MiB Mem\s*:' | tail -n 2 > $tmpdir/euclid-$euclid-cpu-mem &
 	run_cmd "lscpu" > $tmpdir/euclid-$euclid-lscpu &
-	run_cmd "if [[ \$(command -v nvidia-smi) ]]; then nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits; else echo '-1'; fi" > $tmpdir/euclid-$euclid-gpu & # Saves each GPU percentage to a newline
+	run_cmd "if nvidia-smi &>/dev/null; then nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits; else echo '-1'; fi" > $tmpdir/euclid-$euclid-gpu & # Saves each GPU percentage to a newline
 done
 
 # Display outputs:
@@ -33,11 +33,11 @@ while :; do
 	done_count=0
 	for euclid in $SERVERS; do
 		if [[ -s "$tmpdir/euclid-$euclid-cpu-mem" && -s "$tmpdir/euclid-$euclid-lscpu" && -s "$tmpdir/euclid-$euclid-gpu" ]]; then
-			cpu_usage=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^%Cpu' | awk -F'[, ]+' '{print $2+$4+$6}')
-			mem_usage=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^MiB Mem' | awk -F'[, ]+' '{print $8*100/$4}')
-			mem_size=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^MiB Mem' | awk -F'[, ]+' '{print $4/1024}')
-			cores=$(cat $tmpdir/euclid-$euclid-lscpu | grep -e '^Core(s) per socket' -e 'Socket(s)' | paste -s | awk '{print $4*$6}')
-			threads=$(cat $tmpdir/euclid-$euclid-lscpu | grep '^CPU(s)' | awk '{print $2}')
+			cpu_usage=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^%Cpu(s)\s*:' | awk -F'[, ]+' '{print $2+$4+$6}')
+			mem_usage=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^MiB Mem\s*:' | awk -F'[, ]+' '{print $8*100/$4}')
+			mem_size=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^MiB Mem\s*:' | awk -F'[, ]+' '{print $4/1024}')
+			cores=$(cat $tmpdir/euclid-$euclid-lscpu | grep -e '^Core(s) per socket\s*:' -e 'Socket(s)\s*:' | paste -s | awk '{print $4*$6}')
+			threads=$(cat $tmpdir/euclid-$euclid-lscpu | grep '^CPU(s)\s*:' | awk '{print $2}')
 			gpu_file=$(cat $tmpdir/euclid-$euclid-gpu)
 			gpu_string=$(if [[ $gpu_file == "-1" || $gpu_file == *failed* ]]; then echo ""; else printf "   %s%% GPU" "$(echo $gpu_file | tr ' ' ', ')"; fi)
 			printf "euclid-$euclid: %5.1f%% CPU %5.1f%% Mem   %-3d(%-3d) Cores %4.0f GiB%s\n" $cpu_usage $mem_usage $cores $threads $mem_size "$gpu_string"
