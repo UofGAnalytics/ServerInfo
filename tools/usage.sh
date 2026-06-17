be473eec68dcbf1ae3ab5244af935a01a1c52042
Aidan Wansbrough <aidanw@lochan.org>
2026-06-17 11:59:22 +0100

    feat: add colourised thresholds

--- tools/usage.sh
+++ tools/usage.sh
@@ -10,6 +10,9 @@ RETIRED_SERVERS="11 34"
 
 SERVERS=$UNRESTRICTED_SERVERS
 
+COLOUR_THRESHOLDS=(40 75 95)
+
+# If any arguments given, set SERVERS to the input arguments
 if [[ $# > 0 ]]; then
 	SERVERS="$@"
 fi
@@ -25,6 +28,23 @@ for euclid in $SERVERS; do
 	run_cmd "if nvidia-smi &>/dev/null; then nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits; else echo '-1'; fi" > $tmpdir/euclid-$euclid-gpu & # Saves each GPU percentage to a newline
 done
 
+# Set colour of percentages:
+colour(){
+	if (( $(echo "$1 < 0" | bc -l) )); then
+		echo $(tput setaf sgr0) # Default
+	elif (( $(echo "$1 < ${COLOUR_THRESHOLDS[0]}" | bc -l) )); then
+		echo $(tput setaf 2) # Green
+	elif (( $(echo "$1 < ${COLOUR_THRESHOLDS[1]}" | bc -l) )); then
+		echo $(tput setaf 184) # Yellow
+	elif (( $(echo "$1 < ${COLOUR_THRESHOLDS[2]}" | bc -l) )); then
+		echo $(tput setaf 3) # Orange-Yellow
+	elif (( $(echo "$1 <= 100" | bc -l) )); then
+		echo $(tput setaf 1) # Red
+	else
+		echo $(tput setaf sgr0) # Default
+	fi
+}
+
 # Display outputs:
 n_servers=$(echo $SERVERS | wc --words)
 echo "Total CPU Usage; Total Mem Usage; Physical(Logical) Cores; Total Mem; GPU Utilisation per GPU"
@@ -38,9 +58,10 @@ while :; do
 			mem_size=$(cat $tmpdir/euclid-$euclid-cpu-mem | grep -e '^MiB Mem\s*:' | awk -F'[, ]+' '{print $4/1024}')
 			cores=$(cat $tmpdir/euclid-$euclid-lscpu | grep -e '^Core(s) per socket\s*:' -e 'Socket(s)\s*:' | paste -s | awk '{print $4*$6}')
 			threads=$(cat $tmpdir/euclid-$euclid-lscpu | grep '^CPU(s)\s*:' | awk '{print $2}')
+			NORMAL=$(tput setaf sgr0) # Normal colour
 			gpu_file=$(cat $tmpdir/euclid-$euclid-gpu)
-			gpu_string=$(if [[ $gpu_file == "-1" || $gpu_file == *failed* ]]; then echo ""; else printf "   %s%% GPU" "$(echo $gpu_file | tr ' ' ', ')"; fi)
-			printf "euclid-$euclid: %5.1f%% CPU %5.1f%% Mem   %-3d(%-3d) Cores %4.0f GiB%s\n" $cpu_usage $mem_usage $cores $threads $mem_size "$gpu_string"
+			gpu_string=$(if [[ $gpu_file == "-1" || $gpu_file == *failed* ]]; then echo ""; else printf "   "; for usage in $gpu_file; do printf "$(colour $usage)$usage%%$NORMAL, "; done; printf "\b\b GPU"; fi)
+			printf "euclid-$euclid: $(colour $cpu_usage)%5.1f%%$NORMAL CPU $(colour $mem_usage)%5.1f%%$NORMAL Mem   %-3d(%-3d) Cores %4.0f GiB%s\n" $cpu_usage $mem_usage $cores $threads $mem_size "$gpu_string"
 			((done_count++))
 		else
 			printf "euclid-$euclid:\n"
